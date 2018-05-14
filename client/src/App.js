e0920b7baa7c6612c765e4d96ba7ca795f517f13
Neal Rea <nealrea818@gmail.com>
2018-05-14 15:59:35 -0400

    sketch basic outline

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,118 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import banner from './banner-rail.jpg';
 import './App.css';
+import 'react-datepicker/dist/react-datepicker.css';
+
+class Origin extends Component {
+  createOptions = (option) => {
+    return <option>{option}</option>;
+  }
+
+  render() {
+    return (
+      <div className="Origin">
+        <input ref="origin" className="input_bar" placeholder="Origin" list="stations"/>
+        <datalist id="stations">
+          {this.props.stations.map(this.createOptions)}
+        </datalist>
+      </div>
+    );
+  }
+}
+
+class Destination extends Component {
+  createOptions = (option) => {
+    return <option>{option}</option>;
+  }
+
+  render() {
+    return (
+      <div className="Destination">
+        <input ref="destination" className="input_bar" placeholder="Destination" list="stations"/>
+        <datalist id="stations">
+          {this.props.stations.map(this.createOptions)}
+        </datalist>
+      </div>
+    );
+  }
+}
+
+class NumPassengers extends Component {
+  createOptions = (option) => {
+    return <option>{option}</option>;
+  }
+
+  render() {
+    return (
+      <div className="NumPassengers">
+        <input ref="passengers" className="input_bar" placeholder="# of Passengers" list="passengers"/>
+        <datalist id="passengers">
+          {this.props.passengers.map(this.createOptions)}
+        </datalist>
+      </div>
+    );
+  }
+}
+
+class Date extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      startDate: moment(),
+    };
+  };
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  render() {
+    return (
+      <DatePicker className="Date" selected={this.state.startDate} onChange={this.handleChange}/>
+    );
+  }
+}
+
+class TripInfo extends Component {
+  render() {
+    return (
+      <div className="TripInfo">
+        <div className="Stations">
+          <Origin stations={this.props.stations}/>
+          <Destination stations={this.props.stations}/>
+        </div>
+        <div className="PassengerType">
+          <NumPassengers passengers={[1,2,3,4,5,6,7,8]}/>
+          <Date/>
+        </div>
+        <div>
+          <input className="submit" type="submit"/>
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stations: [],
+    };
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={banner} className="Banner"/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TripInfo startDate={this.state.startDate} stations={this.state.stations}/>
       </div>
     );
   }
